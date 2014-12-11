d1bfc4a67ab1860674294a6cd3f809690bd97587
wroblewski-michal <wroblewski-michal@hotmail.com>
2014-12-11 19:39:17 +0100

    kwadratowa
    kwadratowa

---
+++ Kwadratowa/Kwadratowa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kwadratowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta, x0, x1, x2;
+            Console.WriteLine('Podaj a: ');
+            a=Console.Read();
+                        Console.WriteLine('Podaj b: ');
+            b=Console.Read();
+                        Console.WriteLine('Podaj c: ');
+            c=Console.Read();
+            delta=(math)
+
+
+
+        }
+    }
+}
